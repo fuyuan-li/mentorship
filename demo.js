e30b941f1bd264d353a59dffe821ba1c20a2ab57
fuyuan-li <Lfy@gwmail.gwu.edu>
2021-09-11 18:29:28 -0400

    Update demo.js

--- demo.js
+++ demo.js
@@ -1,5 +1,5 @@
 function analyzeClick(){
-  var selected_color = document.querySelector('#color').selectedOptions[0].text //.textetElementById("color").selectedOptions[0].text
+  var selected_color = document.textetElementById("color").selectedOptions[0].text //querySelector('#color').selectedOptions[0].text
   //var selected_place = document.getElementById("place").selectedOptions[0].text
   //var selected_course = document.getElementById("course").selectedOptions[0].text
   alert(selected_color.concat(',', selected_color, ',', selected_color);
