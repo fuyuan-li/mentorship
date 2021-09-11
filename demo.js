2e2e0a2ba0128e537f84573afaa06dadb1728f99
fuyuan-li <Lfy@gwmail.gwu.edu>
2021-09-11 18:05:35 -0400

    Update demo.js

--- demo.js
+++ demo.js
@@ -1,5 +1,7 @@
 function analyzeClick(){
-  var chosen_color
-  chosen_color = document.getElementById("color").value
-  alert(chosen_color);
+  var selected_color = document.getElementById("color")
+  var selected_place = document.getElementById("place")
+  var selected_course = document.getElementById("course")
+  var color = selected_color.options[selected_color.selectedIndex].value
+  alert(color);
 }
