ea3eaf4f53431e79c59f4aafa43dfbbf0b7f6a25
fuyuan-li <Lfy@gwmail.gwu.edu>
2021-09-11 19:09:50 -0400

    Update demo.js

--- demo.js
+++ demo.js
@@ -1,8 +1,24 @@
 function analyzeClick(){
-  
-  var selected_color = document.getElementById("color").selectedOptions[0].text; //querySelector('#color').selectedOptions[0].text
-  var selected_place = document.getElementById("place").selectedOptions[0].text
-  var selected_course = document.getElementById("course").selectedOptions[0].text
-  alert(selected_color.concat(',', selected_place, ',', selected_course));
+  var scores={
+    'red': [1,0,2],
+    'blue': [2,0,1],
+    'golden': [0,2,1],
+    'black': [2,1,0],
+    'shopping': [0,1,1],
+    'beach': [0,0,2],
+    'movie': [0,1,2],
+    'book': [2,0,0],
+    'art': [0,1,1],
+    'math': [2,1,0],
+    'history': [1,2,0],
+    'music': [1,0,1],
+    'lit': [0,1,2],
+  }
+  var selected_color = document.getElementById("color").selectedOptions[0].value; //querySelector('#color').selectedOptions[0].text
+  var selected_place = document.getElementById("place").selectedOptions[0].value;
+  var selected_course = document.getElementById("course").selectedOptions[0].value;
+  yourscore = scores[selected_color].map((e,i)=>e+scores[selected_place][i])
+  yourscore = yourscore.map((e,i)=>e+scores[selected_course][i])
+  alert('We suggest ', 'xxx', yourscore, ' for you!');
   
 }
